a0753f4710fe8db1a3b0696cdcc7066bc9f99b38
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-10-31 01:09:35 +0000

    🎉 started project - boilerplate

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
