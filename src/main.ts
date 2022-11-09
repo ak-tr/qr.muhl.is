46c98284c72dab3ba85276091cfd7dbee066cf55
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-09 14:38:29 +0000

    🎨 added global `isMobile` prop

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.config.globalProperties.$isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+
+app.mount("#app");
